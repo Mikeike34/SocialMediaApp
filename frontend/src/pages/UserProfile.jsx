640ec1dddf6ac860a3e7546ada43b66978227fe5
Mikeike34 <michaeljcantone@gmail.com>
2025-11-05 15:27:27 -0800

    Adjusted User Profile to fit the style of the other pages.

--- frontend/src/pages/UserProfile.jsx
+++ frontend/src/pages/UserProfile.jsx
@@ -1,7 +1,7 @@
 import PostCard from '@/components/PostCard'
 import ProfileFeed from '@/components/ProfileFeed'
 import Sidebar from '@/components/Sidebar'
-import { Avatar, Flex, Heading, HStack, Text, VStack } from '@chakra-ui/react'
+import { Avatar, Box, Flex, Heading, HStack, Text, VStack } from '@chakra-ui/react'
 import React, { useEffect, useState } from 'react'
 
 const UserProfile = () => {
@@ -38,20 +38,63 @@ const UserProfile = () => {
     fetchUser();
   }, [userID, token]);
 
+  //colors
+  const backgroundYellow = '#fdfce8';
+  const green = '#1f574f';
+  const accentYellow = '#f6f8b5';
+  const pink = '#ee98e0';
+  const purple = '#a78dfc';
+  const orange = '#f08853';
+
   return (
-    <Flex minH = '100vh' bgColor = 'gray.600' w = '100%' padding = {0}>
-        <HStack w = '100%' align= 'stretch' mr = {5}>
-          <Sidebar />
-          <Flex 
-            alignItems={'center'}
-            minH = '100vh'
-            flex={'1'}
-            mt = {5}
-            bgColor = 'white'
-            borderRadius={'10px'}
+    <Flex 
+      minH = '100vh' 
+      bg = {green} 
+      p = {{base: 2, md: 4, lg: 6}}
+    >
+        <HStack 
+          w = '100%' 
+          align= 'stretch' 
+          spacing = {{base: 0, md: 4, lg: 6}}
+          flexDir = {{base: 'column', xl: 'row'}}
+        >
+
+          {/*Navigation */}
+          <Box
+            w = {{md: '100px', lg: '150px'}}
+            display = 'flex'
+            justifyContent = 'center'
           >
-           <VStack divideY={'2px'} flexDir={'column'} h= '95vh' w = '100%' align = 'stretch'>
-            <Flex h = '10vh' justify = 'center' align = 'center' >
+            <Sidebar />
+          </Box>
+
+          {/*Content (User's posts)*/}
+          <Flex 
+            alignItems = 'center'
+            minH = '100vh'
+            flex = '1'
+            mt = {{base: 2, xl: 4}}
+            bg = {backgroundYellow}
+            borderRadius = '12px'
+            p = {{base: 2, md: 4}}
+            overflow = 'auto'
+          >
+           <VStack 
+            divideY = '2px'
+            flexDir = 'column' 
+            h= 'full' 
+            w = '100%' 
+            align = 'stretch'
+            spacing = '0'
+           >
+
+            {/*HEader Section */}
+            <Flex 
+              h = {{base: '8vh', md: '10vh'}} 
+              justify = 'center' 
+              align = 'center'
+              px = {{base: 2, md: 4}} 
+            >
               <Avatar.Root>
                 <Avatar.Fallback name = {user?.username} />
                 <Avatar.Image />
@@ -62,7 +105,11 @@ const UserProfile = () => {
               </Flex>
             </Flex>
 
-            <Flex justify = 'center'>
+            {/*Posts */}
+            <Flex 
+              justify = 'center'
+              px = {{base: 2, md: 4}}
+            >
               <ProfileFeed userID = {userID} token = {token} />
             </Flex>
 
